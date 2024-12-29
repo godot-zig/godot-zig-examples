c29d6c475ae3144ca6d4c0f59e8700a90b4f1336
Elvis Zhou <elvis.x.zhou@gmail.com>
2024-12-29 23:37:48 +0800

    Update to godot-zig master

--- build.zig
+++ build.zig
@@ -5,34 +5,33 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
     const godot_path = b.option([]const u8, "godot", "Path to Godot engine binary [default: `godot`]") orelse "godot";
 
+    const godot_dep = b.dependency("godot", .{
+        .target = target,
+        .optimize = optimize,
+        .godot = godot_path,
+
+        // This is the default value, so could be omitted. If you want, you can change it to "double".
+        // You should hardcode this for your project, rather than exposing it as a build option like the godot path.
+        .precision = @as([]const u8, "float"),
+    });
+
     const lib = b.addSharedLibrary(.{
         .name = "example",
-        .root_source_file = .{ .path = "src/Entry.zig" },
+        .root_source_file = b.path("src/Entry.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    const godot_zig_build = @import("./godot-zig/build.zig");
-    const godot = godot_zig_build.createModule(b, target, optimize, godot_path);
-    lib.root_module.addImport("godot", godot);
-
-    // use explicit imports to make jump work properly
-    // todo: remove this once zls get improved
-    var iter = godot.import_table.iterator();
-    while (iter.next()) |it| {
-        lib.root_module.addImport(it.key_ptr.*, it.value_ptr.*);
-    }
-    /////////////////////////////////////////////////
+    lib.root_module.addImport("godot", godot_dep.module("godot"));
 
     b.lib_dir = "./project/lib";
     b.installArtifact(lib);
 
-    const project_path = .{ .path = "./project" };
+    const project_path = b.path("./project");
     const load_cmd = b.addSystemCommand(&.{
         godot_path, "--headless", "--quit", "--editor", "--path",
     });
     load_cmd.addDirectoryArg(project_path);
-    load_cmd.expectExitCode(1);
+    //load_cmd.expectExitCode(0);
     load_cmd.step.dependOn(b.getInstallStep());
     const load_step = b.step("load", "Load project");
     load_step.dependOn(&load_cmd.step);
